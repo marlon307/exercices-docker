53cae4edd476e34f75fae9f9938d3eb72495d0bf
marlon307 <marlon-ramosb@hotmail.com>
2022-01-11 07:56:01 -0300

    project-docker compose finish

--- project-register-user/node-mongo-compose/backend/app.js
+++ project-register-user/node-mongo-compose/backend/app.js
@@ -1,19 +1,36 @@
 const express = require('express');
 const restfull = require('node-restful');
 const server = express();
+const cors = require('cors');
+
+
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+server.use(cors());
 
 const mongoose = restfull.mongoose;
 const PORT = 3000;
 
 // Database
-mongoose.Promise = global.Promise
-mongoose.connect('mongodb://db/mydb');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://mongo/mydb');
 
 // Teste
 server.get('/', (_req, res) => res.send('BackEnd'))
 
-// Start
+// ODM
+const Client = restfull.model('Client', {
+  name: { type: String, required: true }
+});
 
+// Rest API
+Client.methods(['get', 'post', 'put', 'delete']);
+Client.updateOptions({ new: true, runValidators: true });
+
+// Routes
+Client.register(server, '/clients')
+
+// Start
 server.listen(PORT, () => {
   console.log(`Servidor iniciado na porta ${PORT}`);
 });
